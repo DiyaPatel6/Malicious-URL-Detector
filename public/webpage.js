1fc6d63e0ab5b1a6217be4455b8b8ce84fdad9e1
DiyaPatel6 <diyamkpatel6@gmail.com>
2025-03-23 23:28:47 -0400

    regex

--- public/webpage.js
+++ public/webpage.js
@@ -7,17 +7,54 @@ form.addEventListener("submit", function onsubmit(event){
   const formData = new FormData(form);
   const userInput = formData.get("input_box");
   
-  console.log(userInput)
+  if (!userInput.includes("."))
+  {
+    returnMessage.textContent = "Please enter a valid URL";
+    returnMessage.className = "output invalidURL";
+    return;
+  }
 
-  if (userInput === "hi") {
+
+
+
+  threatPoints = 0;
+
+  urlCheck(userInput);
+
+
+  if (threatPoints <= 1) {
     returnMessage.textContent = "✓ Safe Site (proceed) ✓";
-    returnMessage.className = "output safe"
-  } else if (userInput === "hello") {
+    returnMessage.className = "output safe";
+  } else if (2 <= threatPoints <= 5) {
     returnMessage.textContent = "! Suspicious Site (proceed with caution) !";
-    returnMessage.className = "output suspicious"
+    returnMessage.className = "output suspicious";
   } else {
     returnMessage.textContent = "!!! Dangerous Site (should not proceed) !!!";
-    returnMessage.className = "output dangerous"    
+    returnMessage.className = "output dangerous";    
   }
   returnMessage.style.display = "block";   
 })
+
+
+
+function urlCheck(userInput)
+{
+
+  const tld = /\.(info|top|shop|goog|cn|ru|rf|icu|gd|sbs|vip|cc|xyz|app|click|dev|online)$/;  //add more
+  const dashes = /--+/;
+
+  if (tld.test(userInput))
+  {
+    threatPoints += 1;
+  }
+
+  if (dashes.test(userInput))
+  {
+    threatPoints += 1;
+  }
+
+
+  console.log(threatPoints);
+  
+  
+}
