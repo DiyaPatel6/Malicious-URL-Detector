735ec7e7b5e2fb964fcd0ececa9972296812580b
DiyaPatel6 <diyamkpatel6@gmail.com>
2025-03-24 21:24:57 -0400

    API endpoint

--- public/webpage.js
+++ public/webpage.js
@@ -2,7 +2,6 @@ const returnMessage = document.getElementById("returnMessage");
 const form = document.getElementById("check_form");
 let threatPoints = 0; 
 
-
 form.addEventListener("submit", function onsubmit(event){
   event.preventDefault();                
 
@@ -10,8 +9,23 @@ form.addEventListener("submit", function onsubmit(event){
   const userInput = formData.get("input_box");
   threatPoints = 0
 
-  const urlFormat = /^https?:\/\/([0-9a-z-]+\.)+[0-9a-z]{2,6}(\/[0-9a-z-]*)?$/
 
+
+  const inputData = {userInput}; //sends data as a POST
+  const options = {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(inputData)
+  };
+  fetch("/check-url", options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+ 
+
+  const urlFormat = /^https?:\/\/([0-9a-z-]+\.)+[0-9a-z]{2,6}(\/[0-9a-z-]*)?$/
   if (!urlFormat.test(userInput))
   {
     returnMessage.textContent = "Please enter a valid URL";
@@ -19,11 +33,7 @@ form.addEventListener("submit", function onsubmit(event){
     return;
   }
 
-  
- 
-  
   urlCheck(userInput);
-
 
   if (threatPoints == 0) {
     returnMessage.textContent = "✓ Safe Site (proceed) ✓";
@@ -40,10 +50,8 @@ form.addEventListener("submit", function onsubmit(event){
 
 
 
-
 function urlCheck(userInput)
 {
-
  const tld = /\.(org|com|ca|edu|net)\/?$/;  //add more
  const dashes = /--+/;
  const keywords = /(claim|reward|giveaway|app|free|bonus|promo)/i; //i flag for lettercase
@@ -52,29 +60,14 @@ function urlCheck(userInput)
   threatPoints += 1;
   console.log("tld");}
 
-
  if (dashes.test(userInput)) {
   threatPoints += 1;
   console.log("dash");}
-
 
  if (keywords.test(userInput)){
   threatPoints += 1;
   console.log("keyword");}
 
   
-
-
-
-
  console.log(threatPoints);
- 
 }
-
-
-
-
-
-
-
-
--- server.mjs
+++ server.mjs
@@ -1,5 +1,5 @@
 import express from "express"; //imports the Express framework
-import path from "path";  
+import path from "path"; 
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -9,16 +9,24 @@ const app = express(); //create an instance of Express
 const port = 3000; //port is what allows access to your server (3000 is a common choice)
 
 app.get('/', (req, res) => {    //tells server to listen for a 'get' request
-  res.sendFile(path.join(__dirname, '/public', 'webpage.html'));      //when url is visited, it responds with the message 
+ res.sendFile(path.join(__dirname, '/public', 'webpage.html'));      //when url is visited, it responds with the message
 });
 
+
+
+app.use(express.json({limit: '1mb'})); //prevents too much data at once
+
+app.post("/check-url", (req, res) => { //set up route to receive the request
+ console.log("I got a request!");
+ console.log(req.body);
+ res.status(200).json({ message: "Request received!" }); // Respond back with a JSON response
+});
 
 
 
 
 app.use(express.static(path.join(__dirname, '/public')));  //tells server to serve the html, css, js files in the public directory
 
-
 app.listen(port, () => {                         //tells server to start listening on port 3000
-  console.log('server listening on port 3000');  //prints the message
+ console.log('server listening on port 3000');  //prints the message
 });
